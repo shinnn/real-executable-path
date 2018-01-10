770293207ae978d1d6ec0ebbc4e489d98e4f9d75
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-01-10 10:40:57 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update @shinnn/eslint-config and follow its coding rules, for exmaple tab indent
    * switch from istanbul to nyc https://github.com/istanbuljs/nyc

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 const realExecutablePathCallback = require('real-executable-path-callback');
 
 module.exports = function realExecutablePath(executableName, options) {
-  return new Promise((resolve, reject) => {
-    realExecutablePathCallback(executableName, options, (err, filePath) => {
-      if (err) {
-        reject(err);
-        return;
-      }
+	return new Promise((resolve, reject) => {
+		realExecutablePathCallback(executableName, options, (err, filePath) => {
+			if (err) {
+				reject(err);
+				return;
+			}
 
-      resolve(filePath);
-    });
-  });
+			resolve(filePath);
+		});
+	});
 };
--- test.js
+++ test.js
@@ -8,39 +8,39 @@ const test = require('tape');
 process.env.PATH = path.join('node_modules', '.bin');
 
 test('realExecutablePath()', t => {
-  t.plan(6);
+	t.plan(6);
 
-  t.equal(realExecutablePath.name, 'realExecutablePath', 'should have a function name.');
+	t.equal(realExecutablePath.name, 'realExecutablePath', 'should have a function name.');
 
-  realExecutablePath('eslint').then(binPath => {
-    t.equal(
-      binPath,
-      path.resolve('node_modules/eslint/bin/eslint.js'),
-      'should resolve an executable path.'
-    );
-  }).catch(t.fail);
+	realExecutablePath('eslint').then(binPath => {
+		t.equal(
+			binPath,
+			path.resolve('node_modules/eslint/bin/eslint.js'),
+			'should resolve an executable path.'
+		);
+	}).catch(t.fail);
 
-  realExecutablePath('foobarbazqux', null).then(t.fail, err => {
-    t.equal(err.message, 'not found: foobarbazqux', 'should fail when it cannot find a path.');
-  }).catch(t.fail);
+	realExecutablePath('foobarbazqux', null).then(t.fail, err => {
+		t.equal(err.message, 'not found: foobarbazqux', 'should fail when it cannot find a path.');
+	}).catch(t.fail);
 
-  realExecutablePath('eslint', {path: 'foobarbazqux'}).then(t.fail, err => {
-    t.equal(err.message, 'not found: eslint', 'should accept options.');
-  }).catch(t.fail);
+	realExecutablePath('eslint', {path: 'foobarbazqux'}).then(t.fail, err => {
+		t.equal(err.message, 'not found: eslint', 'should accept options.');
+	}).catch(t.fail);
 
-  realExecutablePath(['array'], undefined).then(t.fail, err => {
-    t.equal(
-      err.message,
-      '[ \'array\' ] is not a string. Expected a string of a specific executable name in the PATH.',
-      'should fail when it takes an invalid argument.'
-    );
-  }).catch(t.fail);
+	realExecutablePath([-0], undefined).then(t.fail, err => {
+		t.equal(
+			err.message,
+			'Expected an executable name inside the PATH (<string>), but got a non-string value [ -0 ] (array).',
+			'should fail when it takes an invalid argument.'
+		);
+	}).catch(t.fail);
 
-  realExecutablePath().then(t.fail, err => {
-    t.equal(
-      err.message,
-      'undefined is not a string. Expected a string of a specific executable name in the PATH.',
-      'should fail when it takes no arguments.'
-    );
-  }).catch(t.fail);
+	realExecutablePath().then(t.fail, err => {
+		t.equal(
+			err.message,
+			'Expected an executable name inside the PATH (<string>), but got a non-string value undefined.',
+			'should fail when it takes no arguments.'
+		);
+	}).catch(t.fail);
 });
