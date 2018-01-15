c63c81fee4de846a543d36c0dcc2dcc216a7e722
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-01-15 10:40:57 +0900

    drop real-executable-path-callback dependency

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
 'use strict';
 
-const realExecutablePathCallback = require('real-executable-path-callback');
+const {promisify} = require('util');
+const {realpath} = require('fs');
+
+const inspectWithKind = require('inspect-with-kind');
+const isPlainObj = require('is-plain-obj');
+const which = require('which');
+
+const ARG_ERROR = 'Expected 1 or 2 arguments (<string>[, <Object>])';
+const TYPE_ERROR = 'Expected an executable name inside the PATH (<string>), for exmaple `ls`, `git` and `node`';
+const OPTION_ERROR = 'Expected an option object to be passed to `node-which` https://www.npmjs.com/package/which (`null` by default)';
+const promisifiedRealpath = promisify(realpath);
+const promisifiedWhich = promisify(which);
 
 module.exports = async function realExecutablePath(...args) {
 	const argLen = args.length;
 
-	if (argLen !== 1 && argLen !== 2) {
-		throw new RangeError(`Expected 1 or 2 arguments (<string>[, <Object>]), but got ${
-			argLen === 0 ? 'no' : argLen
-		} arguments.`);
+	if (argLen === 0) {
+		const error = new RangeError(`${ARG_ERROR}, but got no arguments.`);
+		error.code = 'ERR_MISSING_ARGS';
+
+		throw error;
 	}
 
-	return new Promise((resolve, reject) => {
-		realExecutablePathCallback(...args, (err, filePath) => {
-			if (err) {
-				reject(err);
-				return;
-			}
+	if (argLen !== 1 && argLen !== 2) {
+		throw new RangeError(`${ARG_ERROR}, but got ${argLen} arguments.`);
+	}
 
-			resolve(filePath);
-		});
-	});
+	const [cmd, options] = args;
+
+	if (typeof cmd !== 'string') {
+		const error = new TypeError(`${TYPE_ERROR}, but got a non-string value ${inspectWithKind(cmd)}.`);
+		error.code = 'ERR_INVALID_ARG_TYPE';
+
+		throw error;
+	}
+
+	if (cmd.length === 0) {
+		throw new Error(`${TYPE_ERROR.replace(' (<string>)', '')}, but got '' (empty string).`);
+	}
+
+	if (argLen === 1) {
+		return promisifiedRealpath(await promisifiedWhich(cmd));
+	}
+
+	if (!isPlainObj(options)) {
+		throw new TypeError(`${OPTION_ERROR}, but got ${inspectWithKind(options)}.`);
+	}
+
+	if (options.all) {
+		throw new Error(`\`all\` option is not supported, but a value ${
+			inspectWithKind(options.all)
+		} was provided.`);
+	}
+
+	return promisifiedRealpath(await promisifiedWhich(cmd, options));
 };
--- test.js
+++ test.js
@@ -37,6 +37,36 @@ test('realExecutablePath()', async t => {
 	}
 
 	try {
+		await realExecutablePath('');
+	} catch ({message}) {
+		t.equal(
+			message,
+			'Expected an executable name inside the PATH, for exmaple `ls`, `git` and `node`, but got \'\' (empty string).',
+			'should fail when it takes an empty path.'
+		);
+	}
+
+	try {
+		await realExecutablePath('_', [{}]);
+	} catch ({message}) {
+		t.equal(
+			message,
+			'Expected an option object to be passed to `node-which` https://www.npmjs.com/package/which (`null` by default), but got [ {} ] (array).',
+			'should fail when it takes a non-plain options object.'
+		);
+	}
+
+	try {
+		await realExecutablePath('_', {all: true});
+	} catch ({message}) {
+		t.equal(
+			message,
+			'`all` option is not supported, but a value true (boolean) was provided.',
+			'should fail when `all` option is enabled.'
+		);
+	}
+
+	try {
 		await realExecutablePath();
 	} catch ({message}) {
 		t.equal(
